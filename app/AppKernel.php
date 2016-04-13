f27eacc66e815fd8776f2c04d441cf2ec2bb20e4
Kyle Minshall <kyleminshall@gmail.com>
2016-04-13 13:00:30 -0700

    Adding annotations extension to Doctrine

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new APIBundle\APIBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
