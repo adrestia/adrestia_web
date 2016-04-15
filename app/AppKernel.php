f87a59d6d8204ddaf359b9e35888afa13fc9859e
Kyle Minshall <kyleminshall@gmail.com>
2016-04-14 18:45:00 -0700

    Enabling the bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new APIBundle\APIBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new FOS\OAuthServerBundle\FOSOAuthServerBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
