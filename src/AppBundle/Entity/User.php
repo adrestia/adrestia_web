d98375387147427ae6a7c80364626219181b61c4
Kyle Minshall <kyleminshall@gmail.com>
2016-04-21 15:35:04 -0700

    Changing private to protected

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -22,40 +22,39 @@ use Symfony\Component\Security\Core\User\UserInterface;
 class User implements UserInterface, \Serializable
 {
     /**
-     * @var integer
-     *
      * @ORM\Id
-     * @ORM\Column(type="integer", nullable=false, columnDefinition="INT AUTO_INCREMENT UNIQUE")
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
      */
-    private $id;
+    protected $id;
   
     /**
      * @ORM\Column(name="email", type="string", length=255, unique=true)
      * @Assert\NotBlank()
      * @Assert\Email()
      */
-    private $email;
+    protected $email;
   
     /**
      * @ORM\Column(name="email_confirmed", type="boolean") 
      */
-    private $email_confirmed = false;
+    protected $email_confirmed = false;
     
     /**
      * @Assert\NotBlank()
      * @Assert\Length(max=4096)
      */
-    private $plainPassword;
+    protected $plainPassword;
   
     /**
      * @ORM\Column(type="string", length=100)
      */
-    private $password;
+    protected $password;
   
     /**
      * @ORM\Column(name="score", type="integer")
      */
-    private $score = 0;
+    protected $score = 0;
   
     /**
      * @ORM\Column(name="account_status", type="integer")
@@ -67,12 +66,12 @@ class User implements UserInterface, \Serializable
      *    3 – Deactivated by admin
      *    4 – Deleted by user
      */
-    private $account_status = 0;
+    protected $account_status = 0;
   
     /**
      * @ORM\Column(name="suspended", type="boolean")
      */
-    private $suspended = false;
+    protected $suspended = false;
   
     /**
      * @var \Datetime $suspended_date
@@ -81,29 +80,29 @@ class User implements UserInterface, \Serializable
      *
      * The date for when the user is able to access their account again
      */
-    private $suspended_date;
+    protected $suspended_date;
   
     /** 
      * @ORM\Column(name="reports", type="integer")
      */
-    private $reports = 0;
+    protected $reports = 0;
   
     /**
      * @OneToMany(targetEntity="Post", mappedBy="user")
      */
-    private $posts;
+    protected $posts;
   
     /**
      * #@OneToMany(targetEntity="Like", mappedBy="user")
      *
      * This includes dislikes
      */
-     //private $likes;
+     //protected $likes;
   
     /**
      * #@OneToMany(targetEntity="Comment", mappedBy="user")
      */
-    private $comments;
+    protected $comments;
   
     /**
      * @var \Datetime $created
@@ -111,7 +110,7 @@ class User implements UserInterface, \Serializable
      * @Gedmo\Timestampable(on="create")
      * @ORM\Column(type="datetime")
      */
-    private $created;
+    protected $created;
   
     /**
      * @var \DateTime $updated
@@ -119,7 +118,7 @@ class User implements UserInterface, \Serializable
      * @Gedmo\Timestampable(on="update")
      * @ORM\Column(type="datetime")
      */
-    private $updated;
+    protected $updated;
   
     /**
      * @var \DateTime $account_changed
@@ -127,18 +126,18 @@ class User implements UserInterface, \Serializable
      * @ORM\Column(name="account_changed", type="datetime", nullable=true)
      * @Gedmo\Timestampable(on="change", field={"email", "password"})
      */
-    private $account_changed;  
+    protected $account_changed;  
   
     /**
      * @ORM\Column(name="is_active", type="boolean")
      */
-    private $is_active; 
+    protected $is_active; 
     
     /**
      * @ORM\Column(name="api_key", type="guid")
      * @ORM\GeneratedValue(strategy="UUID")
      */
-    private $api_key;
+    protected $api_key;
 
     /**
      * @param mixed $apiKey
