9a8ef61677945302c807a565a6e6bc86cc5504df
Kyle Minshall <kyleminshall@gmail.com>
2016-05-21 04:30:31 -0700

    Massive overhaul of the query. Reduced time from 5 seconds to .05 seconds

--- web/js/index.js
+++ web/js/index.js
@@ -56,12 +56,15 @@ function handleScroll(e) {
     console.log(offset);
     if(element_in_scroll(elem)) {
         $(document).unbind('scroll');
+        console.log("Start");
         $.ajax({
             type: "POST",
             url: document.location.href,
             data: { offset: offset }
         }).done(function( response ) {
+            console.log("Done");
             var posts = $.parseJSON(response.posts);
+            console.log(posts);
             if (posts.length !== 0) {
                 var offset = $(elem).attr('data-post-count');
                 for(post of posts) {
@@ -120,30 +123,30 @@ function correctDate(date) {
 
 function make_post(post, index) {
     // Make the post URL
-    var post_url = "/posts/" + post.id;
+    var post_url = "/posts/" + post.p_id;
     
     // Get the body of the post
-    if(post.body.length > 150) {
-        var body = "<p class=\"post_body\">" + post.body.slice(0, 140) + "<a href=" + post_url + "> More...</a></p>";
+    if(post.p_body.length > 150) {
+        var body = "<p class=\"post_body\">" + post.p_body.slice(0, 140) + "<a href=" + post_url + "> More...</a></p>";
     } else {
-        var body = "<p class=\"post_body\">" + post.body + "</p>";
+        var body = "<p class=\"post_body\">" + post.p_body + "</p>";
     }
     
-    var upvote = post.likes.length > 0 && post.likes[0].is_like ? "blue" : "";
-    var downvote = post.likes.length > 0 && !post.likes[0].is_like ? "pink" : "";
+    var upvote = post.l_id !== undefined && post.l_is_like ? "blue" : "";
+    var downvote = post.l_id !== undefined && !post.l_is_like ? "pink" : "";
     
     // HTML for the post 
-    var post = "<div class=\"row post\" data-post-id=" + post.id + " data-post-count=" + index + "> \
+    var post = "<div class=\"row post\" data-post-id=" + post.p_id + " data-post-count=" + index + "> \
                     <div class=\"small-11 columns body\"> \
                         " + body  + " \
                         <a href=\"#\" class=\"post_report\" data-post-id=" + post.id + ">Report</a> \
-                        <a href=" + post_url + " class=\"view_post\">Comments " + post.comments.length + "</a> \
-                        <span class=\"post_time\" data-post-id=" + post.id + " title=" + formatDate(correctDate(post.created)) + ">" + timeDifference(new Date(), correctDate(post.created)) + "</span> \
+                        <a href=" + post_url + " class=\"view_post\">Comments (" + post.comments + ")</a> \
+                        <span class=\"post_time\" data-post-id=" + post.id + " title=" + formatDate(correctDate(post.p_created)) + ">" + timeDifference(new Date(), correctDate(post.p_created)) + "</span> \
                     </div> \
                     <div class=\"small-1 columns score\"> \
-                        <i class=\"fi-arrow-up post_upvote " + upvote + "\" data-post-id=" + post.id + "></i><br> \
-                        <span class=\"score_number\" data-post-id=" + post.id + ">" + (post.upvotes - post.downvotes) +"</span><br> \
-                        <i class=\"fi-arrow-down post_downvote " + downvote + "\" data-post-id=" + post.id + "></i><br> \
+                        <i class=\"fi-arrow-up post_upvote " + upvote + "\" data-post-id=" + post.p_id + "></i><br> \
+                        <span class=\"score_number\" data-post-id=" + post.p_id + ">" + (post.p_upvotes - post.p_downvotes) +"</span><br> \
+                        <i class=\"fi-arrow-down post_downvote " + downvote + "\" data-post-id=" + post.p_id + "></i><br> \
                     </div> \
                 </div>"
     
