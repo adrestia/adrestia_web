b80968a5da2350ba3be44911c7b31652669f20ad
Kyle Minshall <kyleminshall@gmail.com>
2016-05-20 22:56:42 -0700

    Re-adding createPost Action

--- src/AppBundle/Controller/APIController.php
+++ src/AppBundle/Controller/APIController.php
@@ -302,6 +302,41 @@ class APIController extends Controller
 
        return new JsonResponse(array('status' => 200, 'message' => 'Success on upvoting', 'score' => $score));
     }
+    
+    /**
+     * @Route("/posts", name="api_new_post")
+     * @Method({"POST"})
+     */
+    public function newPostAction(Request $request) 
+    {
+        // Get the User's IP address
+        $post_ip = Utilities::getCurrentIp($this);
+
+        // Need to get the current user based on security acces
+        $user = Utilities::getCurrentUser($this);
+
+        // Get the body of the post from the request
+        $body = $request->get('body');
+    
+        $body = preg_replace("/[\r\n]{2,}/", "\n\n", $body); 
+
+        // We have everything we need now
+        // Time to add the post to the database
+        try {
+            $em = Utilities::getEntityManager($this);
+            $post = new Post;
+            $post->setBody($body);
+            $post->setScore(Utilities::hot(0, 0, new \DateTime('NOW')));
+            $post->setIpAddress($post_ip);
+            $post->setCollege($user->getCollege());
+            $post->setUser($user);
+            $em->persist($post);
+            $em->flush();
+            return new JsonResponse(array('status' => 200, 'message' => 'Success', 'post_id' => $post->getId()));
+        } catch (\Doctrine\DBAL\DBALException $e) {
+            return new JsonResponse(array('status' => 400, 'message' => 'Unable to submit post.'));
+        }
+    }
 
     /**
      * @Route("/{sorting}", name="api_home", requirements={"sorting":"top|new|hot"})
