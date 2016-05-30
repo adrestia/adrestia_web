7f89f8eeb9c4c71eec9a9e784e07dcb91ebf0d30
Kyle Minshall <kyleminshall@gmail.com>
2016-05-30 06:41:25 -0400

    Fixing redirect route for college

--- web/js/college.js
+++ web/js/college.js
@@ -1,4 +1,4 @@
 $('#user_college').change(function() {
   var college = $("#user_college option:selected");
-  window.location = "/college/" + college.val();
+  window.location = "/colleges/" + college.val();
 });
